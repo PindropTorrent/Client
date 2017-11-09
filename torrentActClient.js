37208425a63494e56710c7dc84307535626f3af5
AnkitKSinha <ankit.sinha2000@gmail.com>
2017-11-10 00:58:01 +0530

    command line arg

--- torrentActClient.js
+++ torrentActClient.js
@@ -12,7 +12,7 @@ var emitRequest = function(s, f, n){
 	sck.emit("myevent", {
 		fileId : f,
 		packetNumber : n,
-		sourceIP : 'http://192.168.1.4:4000'
+		sourceIP : 'http://192.168.1.8:4000'
 	});
 	cont = true;
 	console.log("socket emitted to " + s);
@@ -49,23 +49,39 @@ io.on('connection', function (socket) {
 
 
 
-var seeders = ['http://192.168.1.10:3000', 'http://192.168.1.10:3000'];
-fs.readFile('gdata.pin', (err, data)=> {
-	data = JSON.parse(data);
-	var size = 104;
-	var fileId = "g.txt";
-	var sourceIP;
-	i=0;
+var seeders = ['http://192.168.1.10:4000'];
 
-	setInterval(function(){
-		if(cont && i<(size/16)){
-			sourceIP = seeders[(i%2==0)?0:1];
-			emitRequest(sourceIP, fileId, (i+1));
-			cont = false;
-			i++;
-		}
-	},10);
+var download = function(){
+	fs.readFile('gdata.pin', (err, data)=> {
+		data = JSON.parse(data);
+		var size = Number(data.fileSize);
+		var fileId = data.fileId;
+		var sourceIP = seeders[0];
+		i=0;
+		console.log(size);
+
+		setInterval(function(){
+			if(cont && i<(size/16)){
+				// sourceIP = seeders[(i%2==0)?0:1];
+				emitRequest(sourceIP, fileId, (i+1));
+				cont = false;
+				i++;
+			}
+		},10);
+	});
+}
+
+var isSeed = true;
+process.argv.forEach(function (val, index, array) {
+  // console.log(index + ': ' + val);
+  if(val == "download"){
+  	download();
+  	isSeed = false;
+  }
 });
 
+if(isSeed == true){
+	console.log("Seeding Only");
+}
 
 
